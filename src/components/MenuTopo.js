525024558fcdab045e43f1a612b2a08eb389f399
Keu Santos <modulodigital69@gmail.com>
2022-03-16 19:17:11 -0300

    components

---
+++ src/components/MenuTopo.js
@@ -0,0 +1,14 @@
+function MenuTopo({logado=false}) {
+    return (
+        <div>
+            <button style={{
+                display: logado? "none": "flex"
+            }} >
+                    Login
+            </button>
+            <button>Ajuda</button>
+        </div>
+    )
+}
+
+export default MenuTopo;
---
+++ src/components/Rodape.js
@@ -0,0 +1,7 @@
+function Rodape( {autor = "KeuAS"}) {
+    return (
+        <div>Copyleft by {autor} </div>
+    )
+}
+
+export default Rodape;
